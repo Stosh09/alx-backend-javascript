e7b30a0a239e1cc9b3a63b3cbe93a0d410670a87
Stosh09 <stoshochieng@gmail.com>
2024-03-07 08:39:20 +0300

    Feat: Create a new class ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}
+   * @param {Number} maxStudentsSize- The maximum number of
+   * students in a class
+   */
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
